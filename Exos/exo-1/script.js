9adadb2a126ea6db2c14b5af1d73b2ddc72da41b
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-01 14:40:00 +0200

    exercice 1

--- Exos/exo-1/script.js
+++ Exos/exo-1/script.js
@@ -1,2 +1,11 @@
-// you can write js here
-console.log('hello from file');
+
+var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
+//const kelvin = 294;
+var celcius = kelvin - 273;// je prend la valeur de kelvin et soustrait 273
+var fahrenheit = celcius * (9/5) + 32;//je prend la valeur de la variable celcius et mutliplie par 9/5 eme et j'addtionne32 au résultat
+
+
+
+console.log(kelvin);
+console.log(celcius);
+console.log(Math.floor(fahrenheit));
