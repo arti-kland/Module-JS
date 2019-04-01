071f65a2cedc22fbb73177f43915f679b0778a48
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-01 15:03:27 +0200

    exercice 2

--- Exos/exo-2/script.js
+++ Exos/exo-2/script.js
@@ -1,2 +1,20 @@
-// you can write js here
+
+const register = false;//variable de type boolean
+const age = 12;
+let raceNumber = Math.floor(Math.random()*1000);
+
+if (register === false){
+    raceNumber += 1000;
+}
+
+if (age > 18 && register === true){
+    console.log('You will race at 9:30 am, your race Number is : ' +  raceNumber)
+}else if (age > 18 || register === true){
+    console.log('You will race at 11:00 am ' + raceNumber)
+}else if(age < 18 || register === false){
+    console.log('You will race at 12:30 pm ' + raceNumber)
+}else{
+    console.log('Go to see the registration desk');
+}
+
 console.log('exo-2');
