b0a19c238f6918699bb1c5cbb3c4be75ce138fdb
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-02 12:02:41 +0200

    exercice 5

--- Exos/exo-5/exo5.js
+++ Exos/exo-5/exo5.js
@@ -1,3 +1,22 @@
-// you can write js here
+var input ='j\'ai faim !';
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+var resultArray = [];
+var input = input.split('');
 
-console.log('exo-5');
+
+/*for (let i = 0; i < input.length; i++) {
+    if(vowels.indexOf(input[i]) != -1){
+            resultArray.push(input[i]);
+           }
+}*/
+
+  for (let i = 0; i < input.length; i++) {
+    for (let y = 0; y < vowels.length; y++){
+        if(input[i] === vowels[y])
+        resultArray.push(input[i]);
+    }
+  }
+
+  console.log(resultArray.join('').toUpperCase());
+
+  console.log('exo-5');
